9caf391428046b0ecf276f8fb1042fc5fa1fab92
leofinder <imrhyde@yandex.ru>
2024-11-23 11:41:16 +0500

    feat: add PasswordMatchesValidator

---
+++ src/main/java/com/craftelix/filestorage/validation/PasswordMatchesValidator.java
@@ -0,0 +1,13 @@
+package com.craftelix.filestorage.validation;
+
+import com.craftelix.filestorage.dto.user.UserSignupDto;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, UserSignupDto> {
+
+    @Override
+    public boolean isValid(UserSignupDto userSignupDto, ConstraintValidatorContext context) {
+        return userSignupDto.getPassword() != null && userSignupDto.getPassword().equals(userSignupDto.getConfirmPassword());
+    }
+}
