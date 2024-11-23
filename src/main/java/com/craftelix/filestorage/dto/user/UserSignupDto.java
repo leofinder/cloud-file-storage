0e7991ba3816f97811d8f6b4ffa1a4958cd0e0af
leofinder <imrhyde@yandex.ru>
2024-11-23 11:41:39 +0500

    feat: add UserSignupDto

---
+++ src/main/java/com/craftelix/filestorage/dto/user/UserSignupDto.java
@@ -0,0 +1,32 @@
+package com.craftelix.filestorage.dto.user;
+
+import com.craftelix.filestorage.validation.PasswordMatches;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@PasswordMatches
+public class UserSignupDto {
+
+    @Size(min = 3, message = "Username must be at least 3 characters long")
+    @Pattern(
+            regexp = "^[a-z0-9.@_]+$",
+            message = "Username can only contain lowercase letters, digits, and the characters ._@"
+    )
+    private String username;
+
+    @Size(min = 3, message = "Password must be at least 3 characters long")
+    @Pattern(
+            regexp = "^\\S+$",
+            message = "Password must not contain whitespace characters"
+    )
+    private String password;
+
+    private String confirmPassword;
+}
+
