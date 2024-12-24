f1d09d973066ca238b56163f6fe4841d79e22579
leofinder <imrhyde@yandex.ru>
2024-12-24 22:36:35 +0500

    feat: add Prometheus-specific security filter chain and update actuator access rules

--- src/main/java/com/craftelix/filestorage/config/SecurityConfig.java
+++ src/main/java/com/craftelix/filestorage/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.craftelix.filestorage.config;
 
+import com.craftelix.filestorage.entity.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -10,6 +12,20 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    @Order(1)
+    public SecurityFilterChain prometheusFilterChain(HttpSecurity http) throws Exception {
+        http
+                .securityMatcher("/actuator/prometheus")
+                .authorizeHttpRequests(requests -> requests
+                        .anyRequest().hasAuthority(Role.ROLE_ADMIN.name())
+                )
+                .httpBasic(Customizer.withDefaults())
+                .csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -22,6 +38,10 @@ public class SecurityConfig {
                                 "/images/**",
                                 "/signup"
                         ).permitAll()
+                        .requestMatchers(
+                                "/actuator/**"
+                        )
+                        .hasAuthority(Role.ROLE_ADMIN.name())
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
