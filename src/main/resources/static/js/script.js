01d80e398fd6f219854dd76c563964e3130e9f2b
leofinder <imrhyde@yandex.ru>
2024-12-25 09:19:11 +0500

    chore: remove unnecessary comments

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,9 +1,7 @@
-// Обработка клика на Upload Files
 document.getElementById('uploadFilesInput').addEventListener('change', function () {
     document.getElementById('uploadFilesForm').submit();
 });
 
-// Обработка клика на Upload Folders
 document.getElementById('uploadFoldersInput').addEventListener('change', function () {
     document.getElementById('uploadFoldersForm').submit();
 });
@@ -12,15 +10,12 @@ document.addEventListener('DOMContentLoaded', function () {
     const renameModal = document.getElementById('renameModal');
 
     renameModal.addEventListener('show.bs.modal', function (event) {
-        // Получаем кнопку, которая вызвала модальное окно
         const button = event.relatedTarget;
 
-        // Извлекаем данные из data-* атрибутов
         const currentName = button.getAttribute('data-current-name');
         const parentPath = button.getAttribute('data-parent-path');
         const isFolder = button.getAttribute('data-is-folder');
 
-        // Находим элементы в модальном окне и заполняем их
         const modalCurrentNameInput = renameModal.querySelector('#currentFileName');
         const modalParentPathInput = renameModal.querySelector('input[name="parentPath"]');
         const modalIsFolderInput = renameModal.querySelector('input[name="isFolder"]');
