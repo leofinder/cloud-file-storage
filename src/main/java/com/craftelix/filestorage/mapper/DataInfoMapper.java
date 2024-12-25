1b5edba2471b5b88767a1f113c55ace9af5450a7
leofinder <imrhyde@yandex.ru>
2024-12-25 11:17:54 +0500

    refactor: remove unused toEntity method

--- src/main/java/com/craftelix/filestorage/mapper/DataInfoMapper.java
+++ src/main/java/com/craftelix/filestorage/mapper/DataInfoMapper.java
@@ -1,7 +1,6 @@
 package com.craftelix.filestorage.mapper;
 
 import com.craftelix.filestorage.dto.DataInfoDto;
-import com.craftelix.filestorage.dto.DataRequestDto;
 import com.craftelix.filestorage.dto.DataResponseDto;
 import com.craftelix.filestorage.entity.DataInfo;
 import org.mapstruct.Mapper;
@@ -12,8 +11,6 @@ import java.util.List;
 public interface DataInfoMapper {
 
     List<DataResponseDto> toDto(List<DataInfo> dataInfos);
-
-    DataInfo toEntity(DataRequestDto dataRequestDto);
 
     DataInfo toEntity(DataInfoDto dataInfoDto);
 }
